f8fd57ea41a59c76a0682f41b755573f69157eee
zhuang <zhangzhuang@163.com>
2020-09-15 17:12:50 +0800

    添加项目文件。

---
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore.Web
+{
+    public class BookStoreWebAutoMapperProfile : Profile
+    {
+        public BookStoreWebAutoMapperProfile()
+        {
+            //Define your AutoMapper configuration here for the Web project.
+            // ADD a NEW MAPPING
+            CreateMap<Pages.Authors.CreateModalModel.CreateAuthorViewModel,
+                      CreateAuthorDto>();
+
+
+            // ADD THESE NEW MAPPINGS
+            CreateMap<AuthorDto, Pages.Authors.EditModalModel.EditAuthorViewModel>();
+            CreateMap<Pages.Authors.EditModalModel.EditAuthorViewModel,
+                      UpdateAuthorDto>();
+
+            CreateMap<Pages.Books.CreateModalModel.CreateBookViewModel, CreateUpdateBookDto>();
+            CreateMap<BookDto, Pages.Books.EditModalModel.EditBookViewModel>();
+            CreateMap<Pages.Books.EditModalModel.EditBookViewModel, CreateUpdateBookDto>();
+        }
+    }
+}
